9d2e883940238b7b5ab388ad9c21dcc4f00b5b63
Alexandre Borela <alexandre@borela.tech>
2017-06-24 00:50:35 -0300

    Add basic settings

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// Copyright © 2016 Alexandre Borela <alexandre@borela.tech>
+//
+// NOTICE: All information contained herein is, and remains the property of
+// Alexandre Borela and its suppliers, if any. The intellectual and technical
+// concepts contained herein are proprietary to Alexandre Borela and its
+// suppliers and may be covered by patents, patents in process, and are
+// protected by trade secret or copyright law. Dissemination of this information
+// or reproduction of this material is strictly forbidden unless prior written
+// permission is obtained from Alexandre Borela.
+
+module.exports = { snapshotSerializers: [ 'enzyme-to-json/serializer' ]}
